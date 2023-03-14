efcdc61bb4b1ad83a0db2db2232fdd7aa0e48874
Yangs <332207705@qq.com>
2023-03-14 15:35:56 +0800

    实现单行文本无限循环滚动

--- src/views/newsScroll/index.vue
+++ src/views/newsScroll/index.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="news-scroll">123</div>
+  <div class="news-scroll">
+    <header class="header">
+      <div class="content-wrapper">
+        <h2>这是网站标题~</h2>
+      </div>
+    </header>
+
+    <main>
+      <div class="news">
+        <div class="content-wrapper">
+          <TextScroll :texts="texts"></TextScroll>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      <div class="content-wrapper">
+        <p>footer,footer,footer,footer</p>
+      </div>
+    </footer>
+  </div>
 </template>
+
 <script>
+import TextScroll from "./textScroll.vue";
+
 export default {
+  components: {
+    TextScroll,
+  },
   name: "newsScroll",
   data() {
-    return {};
+    return {
+      texts: [
+        "2023年3月14日 13:48:27,--这是消息11111111111111111111!2023年3月14日 13:48:27",
+        "2023年3月14日 13:48:52,==这是消息",
+      ],
+    };
   },
   computed: {},
   created() {},
   methods: {},
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 .news-scroll {
+  header {
+    background-color: #f1f1f1;
+
+    h2 {
+      height: 120px;
+      line-height: 120px;
+    }
+  }
+
+  main {
+    height: calc(100vh - 120px - 60px);
+  }
+
+  footer {
+    background-color: #f1f1f1;
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+  }
 }
 </style>
---
+++ src/views/newsScroll/textScroll.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="text-scroll">
+    <div class="left-text">网站滚动新闻：</div>
+
+    <div class="right-news" ref="rightScrollRef">
+      <!-- 滚动DOM -->
+      <div class="scroll" :style="scrollDOMStyle">
+        {{ currentItem.text }}
+      </div>
+
+      <!-- 计算文本渲染到界面上的宽度 -->
+      <div class="temp-texts">
+        <div class="text" v-for="item in texts" :key="item.index" ref="textRef">
+          {{ item }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TextScroll",
+  props: {
+    texts: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      intervalTimer: null,
+
+      // 开始滚动的位置
+      startDistance: 200,
+      // 滚动元素 left属性值
+      left: 0,
+      //当前滚动项 下标
+      activeIndex: 0,
+      // 文本详情
+      textDetails: [],
+    };
+  },
+  computed: {
+    scrollDOMStyle() {
+      return {
+        left: `${this.left}px`,
+        width: `${this.textDetails[this.activeIndex]?.width ?? 0}px`,
+      };
+    },
+
+    currentItem() {
+      // console.log('yangs=> ',this.textDetails);
+      return this.textDetails[this.activeIndex] ?? {};
+    },
+
+    textList() {
+      return this.texts.map((item, index) => ({
+        id: index,
+        text: item,
+      }));
+    },
+  },
+
+  watch: {
+    texts: {
+      handler() {
+        this.initTextData();
+      },
+      immediate: true,
+    },
+  },
+  mounted() {
+    //简单兼容
+    if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function (fn) {
+        setTimeout(fn, 17);
+      };
+    }
+
+    this.startScroll();
+  },
+  destroyed() {
+    // 清空间隔器
+    if (this.intervalTimer) {
+      clearInterval(this.intervalTimer);
+    }
+  },
+  methods: {
+    initTextData() {
+      this.$nextTick(() => {
+        // 计算文本的显示宽度
+        const textRefs = this.$refs?.textRef ?? [];
+        textRefs.forEach((item) => {
+          const width = item.getBoundingClientRect().width;
+          this.textDetails.push({
+            text: item.innerHTML,
+            width,
+          });
+        });
+
+        // console.log("yangs=> initTextData...", textRefs, this.textDetails);
+
+        const rightScrollRef = this.$refs.rightScrollRef;
+        this.startDistance =
+          rightScrollRef.getBoundingClientRect()?.width ?? 1056;
+          // console.log('yangs=> ',rightScrollRef,this.startDistance);
+      });
+    },
+
+    startScroll() {
+      // 1. 处理参数
+      let index = this.activeIndex;
+      this.left = this.startDistance;
+
+      // 2. 定时器改变left 使文字移动
+      this.intervalTimer = setInterval(() => {
+        this.left -= 1;
+        // 文字左滚动的距离 大于 元素的大小
+        if (-this.left >= this.currentItem.width) {
+          this.left = this.startDistance; // 调整开始位置
+          index = index + 1;
+          if (index >= this.textDetails.length) {
+            // 循环文字列表
+            index = 0;
+          }
+          this.activeIndex = index;
+        }
+      }, 10);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.text-scroll {
+  display: flex;
+  height: 32px;
+  line-height: 32px;
+
+  .left-text {
+    padding: 0 16px;
+    background-color: bisque;
+  }
+}
+.right-news {
+  flex: 1;
+  background-color: azure;
+  position: relative;
+  overflow: hidden;
+  .scroll {
+    // width: 9999px;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+}
+
+.temp-texts {
+  position: fixed;
+  z-index: 999;
+  visibility: hidden;
+  .text {
+    width: max-content;
+  }
+}
+</style>
